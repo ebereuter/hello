d68a3bfe5e26fb5845c0c8e24576459d167c8bc3
ebereuter <bereuterethan@yahoo.com>
2019-08-29 14:06:20 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+  * Author: Ethan Bereuter
+  * Date 8/29/2019
+  *
+  * Hello World Program in C
+  *
+  */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ethan Bereuter"\n");
+
+  return 0;
+}
